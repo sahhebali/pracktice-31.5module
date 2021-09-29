8ac6a8761d0aa27218fc217abebef9d49f80630e
sahhebali <saheb.co21@gmail.com>
2021-09-29 07:02:16 +0530

    practice-1st-commit

---
+++ task-1.js
@@ -0,0 +1,12 @@
+/* ১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো। */
+
+
+var name = 'Md saheb'
+var age = 25;
+
+let dateOfBirth = '12-06-1996';
+let qualification = 'HS';
+
+const toDai = true;
+const sunwakeUp = 'is West';
+console.log(name, age, dateOfBirth, qualification, toDai, sunwakeUp);
